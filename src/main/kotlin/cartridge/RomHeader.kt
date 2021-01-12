a9546c0550cbfd57ee4d905d8404710e46da16bf
Julian Lamprecht <juli.lamprecht@gmail.com>
2021-01-12 12:59:48 +0100

    Updated cartridge

--- src/main/kotlin/cartridge/RomHeader.kt
+++ src/main/kotlin/cartridge/RomHeader.kt
@@ -4,21 +4,21 @@ import ext.toBoolean
 import ext.toHexString
 
 class RomHeader(private val headerBytes: ByteArray) {
-    val headerConstant = headerBytes.copyOfRange(0, 3)
+    private val headerConstant = headerBytes.copyOfRange(0, 3)
     val prgSize = headerBytes[4].toInt() * 0x4000   //times 16KB
     val chrSize = headerBytes[5].toInt() * 0x2000   //times 8 KB
-    val flag6 = headerBytes[6].toInt()
-    val flag7 = headerBytes[7].toInt()
-    val flag8 = headerBytes[8].toInt()
-    val flag9 = headerBytes[9].toInt()
-    val flag10 = headerBytes[10].toInt()
-    val unused = headerBytes.copyOfRange(11, 15)
+    private val flag6 = headerBytes[6].toInt()
+    private val flag7 = headerBytes[7].toInt()
+    private val flag8 = headerBytes[8].toInt()
+    private val flag9 = headerBytes[9].toInt()
+    private val flag10 = headerBytes[10].toInt()
+    private val unused = headerBytes.copyOfRange(11, 15)
 
     val size: Int
         get() = headerBytes.size
 
     val isValid: Boolean
-        get() = headerBytes.toHexString().toUpperCase() == iNesConstant
+        get() = headerConstant.toHexString().toUpperCase() == iNesConstant
 
     val trainerPresent: Boolean
         get() = (flag6 and 0x4).toBoolean()

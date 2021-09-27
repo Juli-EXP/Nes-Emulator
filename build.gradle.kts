4737bd3a61864edde189be7ae1b4196b7254344d
Juli-EXP <juli.lamprecht@gmail.com>
2021-09-27 20:54:20 +0200

    Small config changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
